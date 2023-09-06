5e8319229ddb40310a25ba8bad8cbcd4daa8f779
Aref Orumiehei <areforumiehei@gmail.com>
2023-09-06 13:01:41 +0330

    Add codes into Github

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React , {Component} from 'react'
+
+//Components
+import Navbar from './components/Navbar.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+//Styles
+import styles from "../styles/Navbar.module.css"
+
+//Photo
+import avatar from "../images/photo_2022-09-27_14-08-39.jpg"
+
+const Navbar = () => {
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.listContainer}>
+                <ul className={styles.list}>
+                    <li>Home Page</li>
+                    <li>Products</li>
+                    <li>About Us</li>
+                </ul>
+            </div>
+
+            <div className={styles.logo}>
+                <img src={avatar} alt="avatar" />
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
