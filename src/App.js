a8caba05ffdf90c06d4c2b23c8435b19d148211c
Aref Orumiehei <areforumiehei@gmail.com>
2023-09-07 14:56:07 +0330

    Create Banner and Add Font & Color Pallete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React , {Component} from 'react'
 
 //Components
 import Navbar from './components/Navbar.js'
+import Banner from './components/Banner.js'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar/>
+        <Banner/>
       </div>
     )
   }
---
+++ src/components/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+//Styles
+import styles from '../styles/Banner.module.css'
+
+const Banner = () => {
+    return (
+        <div className={styles.banner}>
+            <img src='https://images.pexels.com/photos/1486222/pexels-photo-1486222.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt=''/>
+            <div className={styles.text}>
+                <div className={styles.left}>
+                    <h2><span className={styles.animationText}>Black</span> Friday is Coming...</h2>
+                </div>
+                <div className={styles.right}>
+                    <h3>Buy now <br/> Pay later</h3>
+                    <p>Lorem ipsum dolor sit amet sed. Kasd diam sit lorem sea ullamcorper feugait dolor sed rebum et.</p>
+                    <button className={styles.btn}>Buy now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
