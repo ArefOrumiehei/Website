77baee527f4bdcbbce5b2a2350555e048791aab7
Aref Orumiehei <areforumiehei@gmail.com>
2023-09-07 15:56:31 +0330

    Change text animation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,10 +9,10 @@ const Banner = () => {
             <img src='https://images.pexels.com/photos/1486222/pexels-photo-1486222.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt=''/>
             <div className={styles.text}>
                 <div className={styles.left}>
-                    <h2><span className={styles.animationText}>Black</span> Friday is Coming...</h2>
+                    <h2><span className={styles.animationText}>Black Friday</span>  is Coming...</h2>
                 </div>
                 <div className={styles.right}>
-                    <h3>Buy now <br/> Pay later</h3>
+                    <h3 className={styles.animationText2}>Buy now <br/> Pay later</h3>
                     <p>Lorem ipsum dolor sit amet sed. Kasd diam sit lorem sea ullamcorper feugait dolor sed rebum et.</p>
                     <button className={styles.btn}>Buy now</button>
                 </div>
